cfbdf5eac94b1a8c0f9f5bf2381cf631e95d736e
john <john3210of@gmail.com>
2022-05-12 22:10:16 +0900

    📰[chore][feat] : gradle devtool 사용 x, yml 파일 셋팅

---
+++ src/main/java/com/shop/mall/Hello.java
@@ -0,0 +1,9 @@
+package com.shop.mall;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+    private String data;
+}
--- src/main/java/com/shop/mall/ShoppingApplication.java
+++ src/main/java/com/shop/mall/ShoppingApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShoppingApplication {
-
     public static void main(String[] args) {
+
+        Hello hello = new Hello();
+        hello.setData("Hello");
+        String data = hello.getData();
+        System.out.println("data123="+data);
+
         SpringApplication.run(ShoppingApplication.class, args);
     }
-
 }
