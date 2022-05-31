6ca45ae01320dcc9ac6226a0e865e30246ccb1e1
john <john3210of@gmail.com>
2022-05-31 21:54:35 +0900

    [fix]사소한 코드 수정

--- src/main/java/com/shop/mall/domain/Timestamped.java
+++ src/main/java/com/shop/mall/domain/Timestamped.java
@@ -17,14 +17,12 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass // 맴버 변수 컬림이 되도록 함
 @EntityListeners(AuditingEntityListener.class) // 변경 되었을 대 자동 기록.
+@JsonSerialize(using = LocalDateTimeSerializer.class)
+@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 public abstract class Timestamped implements Serializable {
     @CreatedDate // 최초 생성 시점
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
 
     @LastModifiedDate // 마지막 변경 시점
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime modifiedAt;
 }
